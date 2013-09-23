eb3560fcb6f450215fc438d39c224bc32f4fcbdf
Alexander Langer <alexander.langer@redrainbow.de>
2013-09-23 23:09:44 +0200

    Add ChangeSet

---
+++ core/src/main/java/org/jeeventstore/core/ChangeSet.java
@@ -0,0 +1,42 @@
+package org.jeeventstore.core;
+
+import java.io.Serializable;
+import java.util.Iterator;
+import java.util.UUID;
+
+/**
+ * A ChangeSet represents a series of events that form a single, consistent
+ * unit within the given event stream. The events within a ChangeSet are
+ * stored in an atomic operation: The events in a ChangeSet can either be
+ * applied to the event stream as a whole, or none of the events are applied.
+ * @author alex
+ */
+public interface ChangeSet extends Serializable {
+
+    /**
+     * Identifies the bucket to which the ChangeSet and the event stream belong.
+     * @return 
+     */
+    String bucketId();
+
+    /**
+     * Identifies the event stream to which the ChangeSet belongs.
+     */
+    String streamId();
+
+    /**
+     * The version of the event stream to which this ChangeSet applies.
+     */
+    long streamVersion();
+
+    /**
+     * A unique identifier for this ChangeSet within the stream.
+     */
+    UUID changeSetId();
+    
+    /**
+     * Gets an iterator to the events contained within this ChangeSet.
+     */
+    Iterator<Serializable> events();
+    
+}
