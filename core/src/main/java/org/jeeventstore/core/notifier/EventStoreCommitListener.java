3d270315cdd3e19e53afe9e50e8d9eb02875c203
Alexander Langer <alexander.langer@redrainbow.de>
2013-10-06 23:40:08 +0200

    Improve Javadoc for EventStoreCommitListener

--- core/src/main/java/org/jeeventstore/core/notifier/EventStoreCommitListener.java
+++ core/src/main/java/org/jeeventstore/core/notifier/EventStoreCommitListener.java
@@ -3,10 +3,17 @@ package org.jeeventstore.core.notifier;
 /**
  * An EventStoreCommitListener is awaiting notifications about
  * changes that have been committed to the event store.
+ * 
  * @author Alexander Langer
  */
 public interface EventStoreCommitListener {
 
+    /**
+     * Receive a notification that a change set has been committed to the
+     * event store.
+     * @param notification The actual notification.
+     * This method must be thread-safe.
+     */
     void receive(EventStoreCommitNotification notification);
     
 }
