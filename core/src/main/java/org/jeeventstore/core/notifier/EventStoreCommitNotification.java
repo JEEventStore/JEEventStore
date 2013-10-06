f74cab76586293549842794dbf330d2b6bc4b779
Alexander Langer <alexander.langer@redrainbow.de>
2013-10-06 23:38:07 +0200

    Improve Javadoc for EventStoreCommitNotification

--- core/src/main/java/org/jeeventstore/core/notifier/EventStoreCommitNotification.java
+++ core/src/main/java/org/jeeventstore/core/notifier/EventStoreCommitNotification.java
@@ -5,10 +5,15 @@ import org.jeeventstore.core.ChangeSet;
 /**
  * An EventStoreCommitNotification is used to inform interested
  * parties about changes that have been committed to the event store.
+ * 
  * @author Alexander Langer
  */
 public interface EventStoreCommitNotification {
 
+    /**
+     * Returns the change set that has been committed to the event store.
+     * @return The committed ChangeSet
+     */
     ChangeSet changes();
     
 }
