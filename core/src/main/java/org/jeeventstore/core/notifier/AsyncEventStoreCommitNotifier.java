7e03f918112f57a1a5b80668e99c7681862d9193
Alexander Langer <alexander.langer@redrainbow.de>
2013-10-13 23:50:33 +0200

    Small non-functional and javadoc changes

--- core/src/main/java/org/jeeventstore/core/notifier/AsyncEventStoreCommitNotifier.java
+++ core/src/main/java/org/jeeventstore/core/notifier/AsyncEventStoreCommitNotifier.java
@@ -42,15 +42,16 @@ import org.jeeventstore.core.ChangeSet;
 
 /**
  * An asynchronous event store commit notifier.
- * Notifies interested listeners asynchronously in a different thread, i.e.,
+ * Notifies registered listeners asynchronously in a different thread, i.e.,
  * the notifyListeners() method does not block.  The notifier begins to
  * notify listeners when the transaction surrounding the call to notifyListeners()
  * commits. If the surrounding transaction is rolled back, the listeners are 
  * not notified.  If the transaction from which notifyListeners() is called
  * is committed, the actual notification is scheduled and will run in a new
- * transaction.  The listeners are therefore running in a new transaction.
+ * transaction.  The listeners are therefore notified in a new transaction
+ * and cannot cause the commit to the event store to fail.
  * If one of the listeners causes the notification-transaction to fail,
- * the notification is re-tried automatically (as per EJB's TimerService semantics).
+ * the notification is re-tried automatically until it succeeds.
  * 
  * The notification order is currently in the order of registration, but this is
  * not a guarantee and might change in the future.
@@ -62,15 +63,19 @@ public class AsyncEventStoreCommitNotifier
         extends AbstractEventStoreCommitNotifier 
         implements EventStoreCommitNotifier {
 
-    private Logger log = Logger.getLogger(AsyncEventStoreCommitNotifier.class.getName());
+    private static final Logger log = Logger.getLogger(AsyncEventStoreCommitNotifier.class.getName());
 
     @Resource
     private TimerService timerService;
 
     private long retryInterval = 100;
 
+    /**
+     * Bean initialization, used to load configuration settings, if any.
+     */
     @PostConstruct
     public void init() {
+        // Test whether a custom retry interval has been configured for this
         try {
             InitialContext ic = new InitialContext();
             Long tmpRetry = (Long) ic.lookup("java:comp/env/retryInterval");
@@ -86,29 +91,28 @@ public class AsyncEventStoreCommitNotifier
     @Override
     @Lock(LockType.READ)
     public void notifyListeners(ChangeSet changeSet) {
-        scheduleListenerNotification(changeSet);
-    }
-    
-    private void scheduleListenerNotification(ChangeSet changeSet) {
         /*
-         * Persistent times are very expensive (requires a 2PC to the
-         * timer storage), and we actually do not need them by the semantics
-         * of notifyListeners()
+         * Schedule the actual notification into a new thread by creating
+         * an create an EJB TimerService interval timer that tries to notify
+         * all listeners until all listeners have been notified and then
+         * cancels itself.  The timer is created transient (non-persistent),
+         * since persistent timers do require a 2-phase-commit to the storage
+         * engine(s) and are therefore very expensive.  We do not need persistent
+         * timers by the semantics of notifyListeners()
          */
         TimerConfig config = new TimerConfig(changeSet, false);
-        // create an interval timer that is cancelled once the notifications
-        // suceeded
         timerService.createIntervalTimer(0, retryInterval, config);
     }
     
     /**
-      * The timer will only fire if the transaction in which the timer was
-      * created was committed successfully.
-      * @param timer 
-      */
+     * Handle the EJB TimerService timeout and notify any registered append
+     * listeners. The timer will only fire if the transaction in which the timer
+     * was created was committed successfully.
+     * @param timer 
+     */
     @Timeout
     @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void notifyAppendListeners(Timer timer) {
+    public void handleTimeout(Timer timer) {
         ChangeSet changeSet = (ChangeSet) timer.getInfo();
         this.performNotification(changeSet);
         timer.cancel();
