d36566ee30aabfd87b1f38a50db6e83a45a7abcb
alex <alexander.langer@redrainbow.de>
2013-10-31 21:08:13 +0100

    Add MockPersistence implementation for tests

---
+++ core/src/test/java/org/jeeventstore/core/store/MockPersistence.java
@@ -0,0 +1,48 @@
+package org.jeeventstore.core.store;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.jeeventstore.core.ChangeSet;
+import org.jeeventstore.core.ConcurrencyException;
+import org.jeeventstore.core.persistence.EventStorePersistence;
+
+/**
+ *
+ * @author Alexander Langer
+ */
+public class MockPersistence implements EventStorePersistence {
+
+    private List<ChangeSet> changeSets = new ArrayList<>();
+
+    @Override
+    public boolean existsStream(String bucketId, String streamId) {
+        return true;
+    }
+
+    @Override
+    public Iterator<ChangeSet> allChanges() {
+        return changeSets.iterator();
+    }
+
+    @Override
+    public Iterator<ChangeSet> getFrom(String bucketId, String streamId, long minVersion, long maxVersion) {
+        int max = Math.min(changeSets.size(), (int) maxVersion);
+        int min = Math.min(changeSets.size(), (int) minVersion);
+        return changeSets.subList(min, max).iterator();
+    }
+
+    @Override
+    public void persistChanges(ChangeSet changeSet) throws ConcurrencyException {
+        // version 0 -> changeset.size() == 0, i.e. wrong if size > 0 
+        // version 1 -> size == 1
+        // etc
+
+        // we add the changeSet first, such that we can ignore the
+        // exception in tests where we actually want to create an invalid stream.
+        changeSets.add(changeSet);
+        if (changeSets.size() != changeSet.streamVersion())
+            throw new ConcurrencyException();
+    }
+    
+}
---
+++ core/src/test/java/org/jeeventstore/core/store/MockPersistenceTest.java
@@ -0,0 +1,81 @@
+package org.jeeventstore.core.store;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+import org.jeeventstore.core.ChangeSet;
+import org.jeeventstore.core.ConcurrencyException;
+import org.jeeventstore.core.persistence.EventStorePersistence;
+import static org.junit.Assert.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+/**
+ *
+ * @author Alexander Langer
+ */
+public class MockPersistenceTest {
+
+    private EventStorePersistence persistence;
+
+    @BeforeMethod
+    public void init() {
+        this.persistence = new MockPersistence();
+        List<ChangeSet> data = TestUtils.createChangeSets("", "", 1, 100);
+        try {
+            for (ChangeSet cs : data)
+                persistence.persistChanges(cs);
+        } catch (ConcurrencyException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    public void test_getFromZero() {
+        Iterator<ChangeSet> it = persistence.getFrom("", "", 0, 10);
+        verify(it, 1, 10);
+        assertTrue(!it.hasNext());
+    }
+
+    @Test
+    public void test_getAllFromZero() {
+        Iterator<ChangeSet> it = persistence.getFrom("", "", 0, Integer.MAX_VALUE);
+        verify(it, 1, 100);
+        assertTrue(!it.hasNext());
+    }
+
+    @Test
+    public void test_getFrom() {
+        Iterator<ChangeSet> it = persistence.getFrom("", "", 34, 57);
+        verify(it, 35, 57);
+    }
+
+    @Test
+    public void test_getFrom_Max() {
+        Iterator<ChangeSet> it = persistence.getFrom("", "", 34, Integer.MAX_VALUE);
+        verify(it, 35, 100);
+        assertTrue(!it.hasNext());
+    }
+
+    private void verify(Iterator<ChangeSet> it, int from, int to) {
+        for (int i = from; i <= to; i++) {
+            System.out.println("compare: " + i);
+            assertTrue(it.hasNext());
+            ChangeSet cs = it.next();
+            assertEquals(i, cs.streamVersion());
+        }
+    }
+
+    @Test
+    private void testConcurrencyFailure() {
+        List<Integer> data = TestUtils.randomdata(10);
+        ChangeSet cs = new DefaultChangeSet("", "", 100, UUID.randomUUID(), data);
+        try {
+            persistence.persistChanges(cs);
+            fail("Should have failed by now");
+        } catch (ConcurrencyException e) {
+            // ok
+        }
+    }
+
+}
