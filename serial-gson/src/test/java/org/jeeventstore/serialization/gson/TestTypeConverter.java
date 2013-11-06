26161b1838e3ab58bcfd06cf51827c37636731ac
alex <alexander.langer@redrainbow.de>
2013-11-06 22:39:40 +0100

    Remove unused imports

--- serial-gson/src/test/java/org/jeeventstore/serialization/gson/TestTypeConverter.java
+++ serial-gson/src/test/java/org/jeeventstore/serialization/gson/TestTypeConverter.java
@@ -2,12 +2,10 @@ package org.jeeventstore.serialization.gson;
 
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import java.lang.reflect.Type;
-import javax.inject.Singleton;
 
 /**
  *
