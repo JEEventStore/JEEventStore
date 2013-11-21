52a22952a9f908ae1b5f7f61dbe8d0c9d3237b8a
Alexander Langer <alexander.langer@redrainbow.de>
2013-11-21 23:10:23 +0100

    Let JPA create/drop tables

--- testutils/create_tables.sh
+++ testutils/create_tables.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 echo "CREATING DATABASES"
-
 mysql -e 'CREATE DATABASE jeeventstore_test;'
-(echo 'USE jeeventstore_test;'; cat ./persistence-jpa/src/main/sql/mysql.sql) | mysql
-mysql -e 'USE jeeventstore_test; TRUNCATE TABLE jeeventstore_test.event_store;'
+# (echo 'USE jeeventstore_test;'; cat ./persistence-jpa/src/main/sql/mysql.sql) | mysql
+# mysql -e 'USE jeeventstore_test; TRUNCATE TABLE jeeventstore_test.event_store;'
 
 psql -U postgres -c 'CREATE DATABASE jeeventstore_test;'
-(echo '\c jeeventstore_test'; \
-	cat ./persistence-jpa/src/main/sql/postgres.sql; \
-	echo 'ALTER SEQUENCE event_store_id_seq RESTART;'; \
-	echo 'TRUNCATE TABLE event_store;' \
-	) | psql -U postgres
+#(echo '\c jeeventstore_test'; \
+#	cat ./persistence-jpa/src/main/sql/postgres.sql; \
+#	echo 'ALTER SEQUENCE event_store_id_seq RESTART;'; \
+#	echo 'TRUNCATE TABLE event_store;' \
+#	) | psql -U postgres
