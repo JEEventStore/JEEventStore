fdc1df9dbce7780e8b08aec7a21028ad73b6bbf3
Alexander Langer <alexander.langer@redrainbow.de>
2013-11-21 23:08:20 +0100

    Tests for the test-serializers

--- testhelpers/src/test/java/org/jeeventstore/serialization/XMLSerializerTest.java
+++ testhelpers/src/test/java/org/jeeventstore/serialization/XMLSerializerTest.java
@@ -1,35 +1,16 @@
 package org.jeeventstore.serialization;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 import org.jeeventstore.EventSerializer;
-import org.jeeventstore.serialization.JavaSerializer;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 /**
  *
  * @author Alexander Langer
  */
-public class JavaSerializerTest {
+public class XMLSerializerTest extends AbstractSerializerTest {
 
-    private List<Serializable> data() {
-        List<Serializable> list = new ArrayList<>();
-        list.add("Hello, World");
-        list.add(12345L);
-        return list;
-    }
-
-    @Test
-    public void test_serialize() throws Exception {
-        EventSerializer sms = new JavaSerializer();
-        List<? extends Serializable> list = data();
-        String res = sms.serialize(list);
-
-        EventSerializer dms = new JavaSerializer();
-        List reconstructed = dms.deserialize(res);
-        Assert.assertEquals(reconstructed, list);
+    @Override
+    EventSerializer serializer() {
+        return new XMLSerializer();
     }
     
 }
